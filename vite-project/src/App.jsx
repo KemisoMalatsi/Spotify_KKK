3483af102c07791434d41e3acd3cc1a720db22fe
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-05-21 14:41:49 +0200

    added React

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>SPOTIFY</h1>
+        <nav>
+          <span>Home</span>
+          <span>Search</span>
+          <span>Theme</span>
+        </nav>
+      </div>
+
+    </>
+  )
+}
+
+export default App
