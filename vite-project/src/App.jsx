884c9465aacd3134b71b4a9cf699b8473d2f0288
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-05-21 15:02:21 +0200

    added items column

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -10,11 +10,22 @@ function App() {
     <>
       <div>
         <h1>SPOTIFY</h1>
-        <nav>
+        <nav id='sidebar-menu'>
           <span>Home</span>
           <span>Search</span>
           <span>Theme</span>
         </nav>
+        <main>
+          <div className='music_list'>
+
+            <p>Title</p>
+            <img src='' alt='image'/>
+            <p>artist</p>
+
+          </div>
+
+
+        </main>
       </div>
 
     </>
